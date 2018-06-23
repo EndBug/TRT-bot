581ae07f3232a7cf9a8f63b0c14787968bcff348
Federico Grandi <fgrandi30@gmail.com>
2018-06-23 21:31:50 +0200

    Moved config.json to cloud

--- src/automation/channel_cleaning.js
+++ src/automation/channel_cleaning.js
@@ -6,7 +6,7 @@ module.exports.clean = (curr, callback = () => {}) => {
   curr.fetchMessages().then((messages) => {
     if (messages.size > 0) curr.bulkDelete(100, true).then(module.exports.clean(curr, callback)).catch((e) => error("channel_cleaning.js", "clean/bulkDelete", e));
     else {
-      setTimeout(() => module.exports.clean(curr), config.cleantimeMin * 60 * 1000);
+      setTimeout(() => module.exports.clean(curr), config.clean * 60 * 1000);
       callback(messages.size);
     }
   }).catch((e) => error("channel_cleaning.js", "clean/fetchMessages", e));
