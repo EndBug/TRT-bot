854c0531c0bd7ce9eecc409a32c9ddac982ad629
Federico Grandi <fgrandi30@gmail.com>
2018-06-25 16:32:11 +0200

    Add .info command

---
+++ src/commands/misc/info.js
@@ -0,0 +1,18 @@
+/*global Commando say*/
+
+module.exports = class InfoCMD extends Commando.Command {
+  constructor(client) {
+    super(client, {
+      name: "info",
+      aliases: ["bot-info", "get-info"],
+      group: "misc",
+      memberName: "info",
+      description: say("info-help")
+    });
+  }
+
+  run(msg) {
+    msg.say(say("info-msg"));
+  }
+
+};
