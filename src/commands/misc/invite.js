7233c6fb783dcd89f479a3a4535cd03653f018b2
Federico Grandi <fgrandi30@gmail.com>
2018-06-25 16:39:56 +0200

    Add .invite

---
+++ src/commands/misc/invite.js
@@ -0,0 +1,18 @@
+/*global Commando say*/
+
+module.exports = class InviteCMD extends Commando.Command {
+  constructor(client) {
+    super(client, {
+      name: "invite",
+      aliases: ["get-invite", "send-invite", "inv"],
+      group: "misc",
+      memberName: "invite",
+      description: say("invite-help"),
+      details: say("invite-note")
+    });
+  }
+
+  run(msg) {
+    msg.answer("invite-msg");
+  }
+};
